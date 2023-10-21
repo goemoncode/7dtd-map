18a8f4a42f79d38007ab924d9d49de4568e8b920
goemoncode <=>
2023-10-21 20:26:01 +0900

    viteのBASE_URLをgithub.ioのURLに修正

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import svgr from 'vite-plugin-svgr';
 export default defineConfig({
   plugins: [react(), svgr(), splitVendorChunkPlugin()],
   root: resolve(__dirname, 'src'),
+  base: '/7dtd-map/',
   publicDir: resolve(__dirname, 'public'),
   envDir: resolve(__dirname),
   build: {
